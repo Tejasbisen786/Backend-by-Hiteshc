cd0186308f1b43868095d150642b3c831a72ed4e
Tejasbisen786 <bisentejas@gmail.com>
2023-09-29 21:00:21 +0530

    some route data added

--- Chai And deploy/index.js
+++ Chai And deploy/index.js
@@ -6,6 +6,21 @@ const express = require("express");
 
 const app = express();
 const port = 4000;
+
+const github_data = {
+  name: "TejasBisen786",
+  repo: 50,
+  url: "Tejasbisen786",
+  key: "gettejaskey",
+};
+
+const myNameinfo = {
+  myName: "Tejas Bisen",
+  clgName: "HVPM COET",
+  ROllNo: "60",
+  Department: "CSE",
+};
+
 app.get("/", (req, res) => {
   res.send("Tejas Bisen");
 });
@@ -21,6 +36,12 @@ app.get("/youtube", (req, res) => {
   res.send("<h2>  Chai And Code </h2>");
 });
 
+app.get("/github", (req, res) => {
+  res.json(github_data);
+});
+app.get("/college", (req, res) => {
+  res.send(myNameinfo);
+});
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
